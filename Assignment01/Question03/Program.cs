c92eb7bdbbc40d6a3fd6c842eda7c2244a271606
Indrajeet Shinde <iamindrajeets242@gmail.com>
2024-12-26 16:49:35 +0530

    Assignment01

--- Assignment01/Question03/Program.cs
+++ Assignment01/Question03/Program.cs
@@ -10,6 +10,62 @@ namespace Question03
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                char choice = 'a';
+
+                Console.WriteLine("Enter operation(e, +, -, *, /)");
+                choice = char.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 'e':
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+                    case '+':
+                        {
+                            Console.WriteLine("Enter 1st number");
+                            int num1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter 2nd number");
+                            int num2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Addition is " + (num1 + num2));
+
+                            break;
+                        }
+                    case '-':
+                        {
+                            Console.WriteLine("Enter 1st number");
+                            int num1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter 2nd number");
+                            int num2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Subtraction is " + (num1 - num2));
+
+                            break;
+                        }
+                    case '*':
+                        {
+                            Console.WriteLine("Enter 1st number");
+                            int num1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter 2nd number");
+                            int num2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Multiplication is " + (num1 * num2));
+
+                            break;
+                        }
+                    case '/':
+                        {
+                            Console.WriteLine("Enter 1st number");
+                            int num1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter 2nd number");
+                            int num2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Division is " + (num1 / num2));
+
+                            break;
+                        }
+                }
+            }
         }
     }
 }
